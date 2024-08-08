b5899f592d029dbb0fb5027688b9220ead6489e0
Vincent Regazzi <vincentregazzi@gmail.com>
2024-08-08 14:36:49 -0500

    initial work on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface Item {
+  id: number,
+  text: string,
+}
+
 function App() {
+  const [listItems, setListItems] = useState<Item[]>([]);
+  const todoItems = listItems.map((entry, index) =>
+    <TodoItem item={entry} setList={setListItems} list={listItems} index={index} key={entry.id} />
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="main-div">
+        <AddNewItemForm setList={setListItems} list={listItems} />
+        <br></br>
+        {todoItems}
+      </div >
+    </>
   );
+}
+
+interface TodoItemProps {
+  item: Item,
+  setList: (list: Item[]) => void,
+  list: Item[],
+  index: number,
+}
+
+function TodoItem(props: TodoItemProps) {
+  const [edit, updateEdit] = useState(false)
+  const [inputText, setEditText] = useState(props.item.text);
+
+  const handleDeleteClick = () => {
+    let newList = [...props.list]
+
+    newList.splice(props.index, 1);
+    props.setList(newList);
+  }
+
+  const handleEditClick = () => {
+    let newList = [...props.list]
+    props.setList(newList);
+    updateEdit(true)
+  }
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    if (!inputText) {
+      return;
+    }
+
+    let newList = [...props.list]
+    newList[props.index] = { text: inputText, id: props.item.id }
+    props.setList(newList)
+
+    updateEdit(false)
+  }
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setEditText(event.target.value)
+  }
+
+  if (!edit) {
+    return (
+      <>
+        <div>
+          <li>
+            <button className="deleteButton" onClick={handleDeleteClick}>X</button>
+            &emsp;
+            <button className="editButton" onClick={handleEditClick}>edit</button>
+            &emsp;
+            {props.item.text}
+          </li>
+        </div>
+      </>
+    )
+  }
+  else {
+    return (
+      <li>
+        <button type="button" className="deleteButton" disabled={true}>X</button>
+        &emsp;
+        <button type="button" className="editButton" disabled={true}>edit</button>
+        &emsp;
+        <form className="editForm" onSubmit={handleSubmit}>
+          <input type="text" value={inputText} onChange={handleChange} autoFocus={true}></input>
+          &emsp;
+          <button className="submitButton">submit</button >
+        </form>
+      </li>
+    )
+  }
+}
+
+interface AddNewItemButtonProps {
+  setList: (list: Item[]) => void,
+  list: Item[],
+}
+
+function AddNewItemForm(props: AddNewItemButtonProps) {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputText(event.target.value)
+  }
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    if (!inputText) {
+      return;
+    }
+
+    let newItem: Item = {
+      id: Math.random() * 100,
+      text: inputText,
+    }
+
+    props.setList([...props.list, newItem])
+    setInputText("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>To do list</p>
+      <input type="text" value={inputText} onChange={handleChange}></input>
+      &emsp;
+      <button className="submitButton">submit</button >
+    </form>
+  )
 }
 
 export default App;
