d163fb8644638efc3f5774ff76ca24f940dd263e
Vincent Regazzi <vincentregazzi@gmail.com>
2024-09-09 09:49:55 -0500

    get tests to run

---
+++ src/AddNewItemForm.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import AddNewItemForm from "./AddNewItemForm";
+import AddNewItemFormProps from "./AddNewItemForm";
+
+describe("Test tests", () => {
+  it("should pass", () => {
+    expect(true).toBeTruthy();
+  });
+});
